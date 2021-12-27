63f193092c7c4d0f19d29c86dfd05d9f13486eaf
Rakesh Baruah <rbaruah@gmail.com>
2021-12-27 06:29:37 -0600

    add contact.js to api route

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,32 @@
 function handler(req, res) {
-    if (req.method === 'POST') {
-        
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    // validate client-side info
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
     }
+
+    // store data in database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    console.log(newMessage);
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", message: newMessage });
+  }
 }
 
 export default handler;
