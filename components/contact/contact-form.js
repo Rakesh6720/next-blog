6017a49b46dc9f34fcbfaaf975beecfa4941aee9
Rakesh Baruah <rbaruah@gmail.com>
2021-12-27 10:29:13 -0600

    reset message form fields after successful sent

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -46,6 +46,9 @@ function ContactForm() {
         message: enteredMessage,
       });
       setRequestStatus("success");
+      setEnteredMessage("");
+      setEnteredEmail("");
+      setEnteredName("");
     } catch (error) {
       setRequestError(error.message);
       setRequestStatus("error");
